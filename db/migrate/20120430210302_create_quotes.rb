ed0b1272596535942edb15581b27987dd4e5f22e
Henrik Wiberg <mrhenkan@gmail.com>
2013-06-30 14:58:09 +0200

    Updated migrations.

--- db/migrate/20120430210302_create_quotes.rb
+++ db/migrate/20120430210302_create_quotes.rb
@@ -2,8 +2,9 @@ class CreateQuotes < ActiveRecord::Migration
   def change
     create_table :quotes do |t|
       t.text :text
-      t.source :string
+      t.string :url
       t.integer :user_id
+      t.integer :source_id
       t.timestamps
     end
   end

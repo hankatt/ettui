29ed8aeb0469c920766c8b99f4ef08b43d945f83
Henrik Wiberg <mrhenkan@gmail.com>
2013-12-08 23:22:33 +0100

    Updated the boards design and added profile information.

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -32,8 +32,9 @@ class BoardsController < ApplicationController
         end
 
         @board = Board.find(params[:id])
-        puts("params: " << params.to_s)
         @quotes = @board.complex_find_by(params)
+
+        
 
          # Get a list of all sources for this users quotes
         @sources = Source.where(:id => @board.quotes.pluck(:source_id))

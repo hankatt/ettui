8178c764c3c22cac849053f88c524c133b6e6e0b
Henrik Wiberg <mrhenkan@gmail.com>
2014-06-30 12:41:08 +0200

    Updated the bookmarklet to use reasonably similar Google Web Fonts.

--- public/bookmarklet_local.js
+++ public/bookmarklet_local.js
@@ -1,3 +1,19 @@
+WebFontConfig = {
+	google: { 
+		families: [ 'Crimson+Text::latin', 'Open+Sans:600:latin' ]
+	}
+};
+
+(function() {
+	var wf = document.createElement('script');
+	wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+	  '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	wf.type = 'text/javascript';
+	wf.async = 'true';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wf, s);
+})();
+
 jquery = document.createElement("script");
 jquery.src = "https://code.jquery.com/jquery-2.1.1.min.js";
 document.body.appendChild(jquery);

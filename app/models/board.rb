d9b2ff5d07ed08334df8ab9d94bba94097b9729a
Henrik Wiberg <mrhenkan@gmail.com>
2014-08-12 17:27:23 +0200

    Moved search above the quotes to more directly create the connection that you search the visible quotes.

--- app/models/board.rb
+++ app/models/board.rb
@@ -6,13 +6,13 @@ class Board < ActiveRecord::Base
 
 	def complex_find_by(params)
 		if params[:search] && params[:source_ids]
-            self.quotes.where("source_id IN (?) AND text LIKE ?", params[:source_ids], "%#{params[:search]}%")
+            self.quotes.order(created_at: :desc).where("source_id IN (?) AND text LIKE ?", params[:source_ids], "%#{params[:search]}%").order(:created_at)
         elsif params[:search]
-            self.quotes.where("text LIKE ?", "%#{params[:search]}%")
+            self.quotes.order(created_at: :desc).where("text LIKE ?", "%#{params[:search]}%").order(:created_at)
         elsif params[:source_ids]
-            self.quotes.where("source_id IN (?)", params[:source_ids])
+            self.quotes.order(created_at: :desc).where("source_id IN (?)", params[:source_ids]).order(:created_at)
         else
-            self.quotes.to_a
+            self.quotes.order(created_at: :desc).to_a
         end
 	end
 end

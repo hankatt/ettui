ce2ddb690b15a2eed5d4914c42318e158a2e7003
Henrik Wiberg <mrhenkan@gmail.com>
2015-03-01 19:13:49 +0100

    Made a HeaderHelper to support the various header title and button states.

--- app/helpers/header_helper.rb
+++ app/helpers/header_helper.rb
@@ -1,7 +1,7 @@
 module HeaderHelper
 	def left_header_element
 		if params[:controller].eql?("users")
-  			link_to @left_title, :back, :class => "quotes-title indent"
+  			link_to @left_title, boards_path, :class => "quotes-title indent"
   		else
   			content_tag :h4, :class => "quotes-title indent" do
   				@left_title

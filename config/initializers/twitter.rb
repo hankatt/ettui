0e889e32607cf2c6a9b75437e8e7e3345ad2e9d8
Hannes Gustafsson <hnnsgstfssn@gmail.com>
2015-06-28 00:21:40 +0200

    Replace hard tabs with soft spaces

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,4 @@
 Twitter.configure do |config|
-	config.consumer_key = "VVeSODAiQmCGaqcmbJBZjg"
-	config.consumer_secret = "3gaqnVLxNybMS9EdtVbJRtfdg2acyTKamvotNQXnHGU"
+  config.consumer_key = "VVeSODAiQmCGaqcmbJBZjg"
+  config.consumer_secret = "3gaqnVLxNybMS9EdtVbJRtfdg2acyTKamvotNQXnHGU"
 end
--- lib/readability_parser.rb
+++ lib/readability_parser.rb
@@ -1,21 +1,21 @@
 class ReadabilityParser
 
-	# URL to Readability's Parser API
-	READABILITY_URI = URI('http://www.readability.com/api/content/v1/parser')
-	READABILITY_TOKEN = '34831792edfc0cf8e42b3e82086f00970a53407b'
+  # URL to Readability's Parser API
+  READABILITY_URI = URI('http://www.readability.com/api/content/v1/parser')
+  READABILITY_TOKEN = '34831792edfc0cf8e42b3e82086f00970a53407b'
 
-	def self.parse_url(url)
-		# URL to parse with the Parser API
-		url = URI.unescape(url)
+  def self.parse_url(url)
+    # URL to parse with the Parser API
+    url = URI.unescape(url)
 
-		# Format params to web form format
-		READABILITY_URI.query = URI.encode_www_form({:url => url, :token => READABILITY_TOKEN })
+    # Format params to web form format
+    READABILITY_URI.query = URI.encode_www_form({:url => url, :token => READABILITY_TOKEN })
 
-		# Readability Parser response
-		response = Net::HTTP.get_response(READABILITY_URI)
-		parsed = ActiveSupport::JSON.decode(response.body)
+    # Readability Parser response
+    response = Net::HTTP.get_response(READABILITY_URI)
+    parsed = ActiveSupport::JSON.decode(response.body)
 
-		# Returns parsed
-		parsed
-	end
+    # Returns parsed
+    parsed
+  end
 end

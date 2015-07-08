bb72102e2ff6619c04c426745becc5724708b804
Hannes Gustafsson <hnnsgstfssn@gmail.com>
2015-07-08 19:24:03 +0200

    Indentation fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
-   protect_from_forgery
+  protect_from_forgery
 
-   helper_method :current_user, :record_user_activity
+  helper_method :current_user, :record_user_activity
 
-   def set_titles left, right
+  def set_titles left, right
     @left_title = left
     @right_title = right
   end
 
-  private
+private
 
   def current_user
     @current_user ||= User.find(cookies[:user_id]) if cookies[:user_id]

92b3d8355a4ba41bb12d24a7353a3e787c6c69fb
Henrik Wiberg <mrhenkan@gmail.com>
2015-11-08 15:20:23 +0100

    Added sidebar menu slidein animation. Aligned some paddings and responsive discrepancies.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,11 @@
 
 $(document).on('ready DOMChange', function() {
     $(".js-toggle-sidebar").on('click', function() {
+        list_to_toggle = $(this).next('ol');
+        list_to_toggle.slideToggle('fast');
+    });
+
+    $(".js-toggle-menu").on('click', function() {
         $(".c-sidebar_container").toggleClass('is-open');
     });
 

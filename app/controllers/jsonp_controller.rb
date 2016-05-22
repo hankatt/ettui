aaed5d77a3744a0406f56987152ad58c970c629a
Henrik Wiberg <mrhenkan@gmail.com>
2016-05-22 14:35:04 +0200

    Factored JSONP Tag and Quote creation to TagHandlar and QuoteHandler.

--- app/controllers/jsonp_controller.rb
+++ app/controllers/jsonp_controller.rb
@@ -2,31 +2,30 @@ class JsonpController < ApplicationController
   def tag_creation
     if params[:user_token]
 
-      @user = User.find_by(token: params[:user_token])
+      # Ensures the Tag exists
+      @tag = TagHandler.find_or_initialize(params[:tag])
+
+      # Add tag to the Quote's tags
       @quote = Quote.find(params[:quote_id])
-      @board = @user.board
-
-      # Important to downcase for searchability
-      @tag = Tag.find_or_initialize_by(name: URI.unescape(params[:tag]).downcase)
-
-      if @tag.new_record? && @tag.valid?
-        @tag.save
-        @tag.is_new = true
-      else
-        @tag.is_existing = true
-      end
       @quote.tags << @tag
-      @tags = @user.unique_tags # Get all the users unique tags from the users board
+
+      # Get all the users unique tags from the users board
+      @user = User.find_by(token: params[:user_token])
+      @tags = @user.unique_tags
+
+      # Return a set with tags inersecting the user's tags and the quote's.
+      # Mark all these as existing.
+      (@tags & @quote.tags).each do
+        |tag| tag.is_existing = true
+      end
     end
 
-
     respond_to do |format|
-      (@tags & @quote.tags).each do |tag| tag.is_existing = true end
-      # @tags.detect{ |tag| tag.id == @tag.id }.is_existing = true
+      # Sending Bookmarklet partial as stringified HTML to Bookmarklet.
       html = render_to_string(partial: "bookmarklet", layout: false, locals: {
         tags: @tags,
         title: "Added the tag ##{@tag.name}.",
-        subtitle: "Tags help you to easily find quotes later."
+        subtitle: "Tags help you find quotes later."
       })
 
       # Respond with data{…} sent to the function added(…) in the bookmarklet
@@ -35,53 +34,36 @@ class JsonpController < ApplicationController
   end
 
   def quote_creation
-    @user = User.find_by(token: params[:user_token])
+    if params[:user_token]
 
-    url = URI.unescape(params[:url])
-    hostname = HostnameParser.parse(url)
-    favicon_url = URI.unescape(params[:favicon])
-    text = URI.unescape(params[:text])
+      @user = User.find_by(token: params[:user_token])
+      @quote = QuoteHandler.create_for_user(params, @user.id)
+      callback = params[:callback]
+      html = ""
 
-    @source = Source.find_by(hostname: hostname) ||
-              Source.create(hostname: hostname, favicon: favicon_url)
+      respond_to do |format|
+        # If the quote was successfully saved
+        if @quote.save
+          @user.board.quotes << @quote
+          @tags = @user.unique_tags
 
-    readability_response = ReadabilityResponse.new(params[:url])
+          html = render_to_string(partial: "bookmarklet", layout: false, locals: {
+            tags: @tags,
+            title: "Saved.",
+            subtitle: "Adding tags helps you find a quote again."
+          })
+        else
+          callback = "failed"
+          html = render_to_string(partial: "bookmarklet", layout: false, locals: {
+            tags: nil,
+            title: "Failed to save.",
+            subtitle: "This is most likely our fault."
+          })
+        end
 
-    @quote = Quote.new(
-      text: text,
-      url: url,
-      readability_title: readability_response.title,
-      readability_author: readability_response.author,
-      user_id: @user.id,
-      source_id: @source.id
-    )
-
-    # Variables for the respond_to do |format|, defined in the if else.
-    html = ""
-    callback = params[:callback]
-    if @quote.save
-
-      @user.board.quotes << @quote
-      @tags = @user.unique_tags
-
-      html = render_to_string(partial: "bookmarklet", layout: false, locals: {
-        tags: @tags,
-        title: "The quote was saved.",
-        subtitle: "Adding tags helps you to easily find it again."
-      })
-    else
-
-      callback = "failed"
-
-      html = render_to_string(partial: "bookmarklet", layout: false, locals: {
-        tags: nil,
-        title: "The quote failed to save.",
-        subtitle: "This is most likely an error on our end."
-      })
-    end
-
-    respond_to do |format|
-      format.json{ render json: { html: html, quote_id: @quote.id }, callback: callback, layout: false }
+        # Respond with data{…} sent to the function added(…) in the bookmarklet
+        format.json { render json: { html: html, quote_id: @quote.id }, callback: callback, layout: false }
+      end
     end
   end
 end

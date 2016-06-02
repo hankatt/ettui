660dc10ecd5e52caadcd8822b958eb3fb1e8bd67
Henrik Wiberg <mrhenkan@gmail.com>
2016-06-03 00:22:52 +0200

    Fixed wrong Postmark TemplateID.

--- config/application.rb
+++ config/application.rb
@@ -50,6 +50,8 @@ module Well
     # config.assets.enabled = true
 
     # Email with Postmark
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :postmark
     config.action_mailer.postmark_settings = { :api_key => ENV["POSTMARK_API_TOKEN"] }
 

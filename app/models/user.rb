390310141fa2b8d02ceeff28840c321a11a7d58f
Henrik Wiberg <mrhenkan@gmail.com>
2016-06-03 01:06:15 +0200

    Allow non authorized users to access the request new password page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   attr_accessor :password, :password_confirmation
 
   # Encrypt password before saving it to the database
-  before_save :encrypt_password, :clear_password_reset_token
+  before_save :encrypt_password
 
   # If save was successful, set a token for the user
   after_save :initialize_user
@@ -86,13 +86,6 @@ class User < ActiveRecord::Base
     if password.present?
       self.password_salt = BCrypt::Engine.generate_salt
       self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
-    end
-  end
-
-  def clear_password_reset_token
-    if password_reset_token.present?
-      self.password_reset_token = nil
-      self.password_reset_sent_at = nil
     end
   end
 

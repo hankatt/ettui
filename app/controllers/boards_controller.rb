5bc8ec1126c507f66a7a6cad877b364ca89fc15b
Henrik Wiberg <mrhenkan@gmail.com>
2016-06-17 00:27:03 +0200

    Demo users were able to view all boards. Boards are now private.

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,25 +2,32 @@ class BoardsController < ApplicationController
   before_action :authorize
 
   def show
-    @search = Search.new(params[:search])
+    
     @board = Board.find(params[:id])
-    @sources = Source.where(id: @board.quotes.pluck(:source_id))
 
-    # Fetch the Quotes according to the Search query, Source filters and Tag filters
-    @quotes = Quote.in_board(@board).filter_by_text(@search.query).filter_by_source_ids(@search.source_ids)
-    if !@search.tag_ids.empty?
-      @quotes = @quotes.joins(:tags).merge(Tag.filter_by_ids(@search.tag_ids))
-    end
+    if current_user.board.id.eql?(@board.id)
+      @search = Search.new(params[:search])
+      @board = current_user.board
+      @sources = Source.where(id: @board.quotes.pluck(:source_id))
 
-    # Only return uniq entries, no duplicate hits
-    @quotes = @quotes.uniq.reverse_order
+      # Fetch the Quotes according to the Search query, Source filters and Tag filters
+      @quotes = Quote.in_board(@board).filter_by_text(@search.query).filter_by_source_ids(@search.source_ids)
+      if !@search.tag_ids.empty?
+        @quotes = @quotes.joins(:tags).merge(Tag.filter_by_ids(@search.tag_ids))
+      end
 
-    if @search.query.empty?
-      @left_title = ""
-      @quotes_container_title = "All quotes"
+      # Only return uniq entries, no duplicate hits
+      @quotes = @quotes.uniq.reverse_order
+
+      if @search.query.empty?
+        @left_title = ""
+        @quotes_container_title = "All quotes"
+      else
+        @left_title = ""
+        @quotes_container_title = "All quotes containing \"#{@search.query}\""
+      end
     else
-      @left_title = ""
-      @quotes_container_title = "All quotes containing \"#{@search.query}\""
+      redirect_to :root
     end
   end
 end

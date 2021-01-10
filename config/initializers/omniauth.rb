24d77bb0cfe11aa0b0d519acfe3cf14013a8d196
Henrik Wiberg <mrhenkan@gmail.com>
2021-01-10 18:00:47 +0100

    Adding explicit options params in Omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'VVeSODAiQmCGaqcmbJBZjg', '3gaqnVLxNybMS9EdtVbJRtfdg2acyTKamvotNQXnHGU'
+  provider :twitter, 'VVeSODAiQmCGaqcmbJBZjg', '3gaqnVLxNybMS9EdtVbJRtfdg2acyTKamvotNQXnHGU',
+  	{
+      :secure_image_url => 'true',
+      :image_size => 'original'
+    }
 end

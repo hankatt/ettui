1bf728800d1b58966f20e0a073a9397c0ec2785b
Henrik Wiberg <mrhenkan@gmail.com>
2026-05-10 20:55:55 +0200

    Add json_quote_removal to protect_from_forgery

--- app/controllers/json_controller.rb
+++ app/controllers/json_controller.rb
@@ -1,5 +1,5 @@
 class JsonController < ApplicationController
-    protect_from_forgery except: [:json_quotes, :json_quote_creation, :json_tag_creation, :json_sign_out]
+    protect_from_forgery except: [:json_quotes, :json_quote_creation, :json_quote_removal, :json_tag_creation, :json_sign_out]
 
     def json_demo
       @user = CreateGuest.create
